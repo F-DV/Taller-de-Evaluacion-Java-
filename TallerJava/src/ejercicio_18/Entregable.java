64553762836d01a5393c4595639c2122a6e5fe89
Felipe <fquiceno1@misena.edu.co>
2022-02-15 08:14:02 -0500

    creamos la interface Entregable maquetada

---
+++ TallerJava/src/ejercicio_18/Entregable.java
@@ -0,0 +1,16 @@
+package ejercicio_18;
+
+public interface Entregable {
+
+    public static void entregar(){
+    }
+    public static void devolver(){
+
+    }
+    public static boolean isEntregado(){
+
+        return true;
+    }
+    public static void compareTo(Object o){
+    }
+}
