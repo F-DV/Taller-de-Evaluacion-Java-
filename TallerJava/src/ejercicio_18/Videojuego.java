626fa31bb177e126234ec6b6e2d59356f4d933fb
Felipe <fquiceno1@misena.edu.co>
2022-02-15 07:17:52 -0500

    Se crea la clase Video  con todos los lineamientos del documento (constructores,toString,getters and setter)

---
+++ TallerJava/src/ejercicio_18/Videojuego.java
@@ -0,0 +1,68 @@
+package ejercicio_18;
+
+public class Videojuego {
+
+    private String titulo = "";
+    private int horasEstimadas = 0;
+    private boolean entragado = false;
+    private String genero = "";
+    private String compañia = "";
+
+    public Videojuego() {
+    }
+
+    public Videojuego(String titulo, int horasEstimadas) {
+        this.titulo = titulo;
+        this.horasEstimadas = horasEstimadas;
+    }
+
+    public Videojuego(String titulo, int horasEstimadas, String genero, String compañia) {
+        this.titulo = titulo;
+        this.horasEstimadas = horasEstimadas;
+        this.genero = genero;
+        this.compañia = compañia;
+    }
+
+    @Override
+    public String toString() {
+        return "Videojuego{" +
+                "titulo='" + titulo + '\'' +
+                ", horasEstimadas=" + horasEstimadas +
+                ", entragado=" + entragado +
+                ", genero='" + genero + '\'' +
+                ", compañia='" + compañia + '\'' +
+                '}';
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getHorasEstimadas() {
+        return horasEstimadas;
+    }
+
+    public void setHorasEstimadas(int horasEstimadas) {
+        this.horasEstimadas = horasEstimadas;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCompañia() {
+        return compañia;
+    }
+
+    public void setCompañia(String compañia) {
+        this.compañia = compañia;
+    }
+}
